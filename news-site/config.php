b23eab37b948ce388353b32edecfa986918a7d34
minthukha772 <minthukhabillionarie@gmail.com>
2023-04-04 12:48:29 +0630

    commit

--- news-site/config.php
+++ news-site/config.php
@@ -1,25 +1,26 @@
 <?php
-
 /*$hostname = "http://localhost/news-site";
 
 $conn = mysqli_connect("localhost","root","","news-site") or die("Connection failed : " . mysqli_connect_error());*/
 
 
-    $host = '127.0.0.1';
-    $db   = 'news-site';
-    $user = 'root';
-    $pass = '123';
-    $charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$host = '127.0.0.1';
+$db   = 'news-site';
+$user = 'root';
+$pass = '123';
+$charset = 'utf8mb4';
 
-    try{
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-        $pdo = new PDO($dsn,$user,$pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try{
 
-    }catch(PDOException $e){
-        throw new PDOException($e->getMessage());
-    }
+    $pdo = new PDO($dsn,$user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+    throw new PDOException($e->getMessage());
+}
+
 
 ?>
